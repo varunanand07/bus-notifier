9eb64789c9ec56679e49ae44f8dd4f27f96d6bfd
Naza <uzoukwuc@tcd.ie>
2023-11-15 18:42:50 +0000

    Add spring starter template

---
+++ src/main/java/ie/tcd/scss/busnotifier/BusnotifierApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.busnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusnotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusnotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/tcd/scss/busnotifier/BusnotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.busnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusnotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
