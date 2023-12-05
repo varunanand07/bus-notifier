7abc1cc57ccdedeb905ef0b355bdb0914254e730
Naza <uzoukwuc@tcd.ie>
2023-12-05 20:07:21 +0000

    Refactor and cleanup tests

--- src/test/java/ie/tcd/scss/busnotifier/GtfsTests.java
+++ src/test/java/ie/tcd/scss/busnotifier/GtfsTests.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LessThanRawTests {
+class GtfsTests {
 	@LocalServerPort
 	private int port;
 
