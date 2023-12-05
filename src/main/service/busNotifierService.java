1abf832f054e64b6f1ac597f87cfa6d386eca505
RalfDuli <ralfduli24@gmail.com>
2023-12-05 13:07:27 +0000

    API Fetching service only has to download ZIP file once

--- src/main/service/busNotifierService.java
+++ src/main/service/busNotifierService.java
@@ -6,24 +6,66 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.net.URL;
 import java.net.URI;
+import java.io.*;
+import java.util.zip.*;
+import java.nio.file.*;
 
 @Service
 public class busnotifierService {
 
     private final String gtfsRealtimeUrl = "https://www.transportforireland.ie/transitData/Data/GTFS_Dublin_Bus.zip";
+    private final String dataDirectory = 'data/';
 
-    public void getTimetableForStop(String stopId) {
+    public TripUpdate getTimetableForStop(String stopId) {
+
         try {
-            URL url = new URL(gtfsRealtimeUrl);
-            FeedMessage feed = FeedMessage.parseFrom(url.openStream());
+            File localDataFile = new File(dataDirectory)
+            if (!localDataFile.exists()) {
+                Files.createDirectories(localDataFile)
+                downloadZipFile();
+            }
+            FeedMessage feed = FeedMessage.parseFrom(
+                new FileInputStream(localDataFile)
+            );
             for (FeedEntity entity : feed.getEntityList()) {
                 if (entity.hasTripUpdate()) {
-                    System.out.println(entity.getTripUpdate());
+                    return entity.getTripUpdate();
                 }
             }
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
+            return null;
         }
 
     }
+
+    private void downloadZipFile() {
+
+        URL url = new URL(gtfsRealtimeUrl);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod('GET');
+
+        InputStream in = connection.getInputStream();
+        ZipInputStream zipIn = new ZipInputStream(in);
+
+        ZipEntry entry = zipIn.getNextEntry();
+        String entryName = entry.getName();
+
+        while (entry != null) {
+            if (!entry.isDirectory()) {
+                FileOutputStream fos = new FileOutputStream(dataDirectory + entryName);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+            String entryName = entry.getName();
+        }
+
+    }
+
 }
